2a5ffc861ef2d7f64d8bbc19f2cf157a1a0fabfc
ChibiKev <k3v.ch3n@gmail.com>
2022-01-09 12:39:39 -0500

    Removed Error Issues With Code

--- solution.py
+++ solution.py
@@ -15,30 +15,42 @@ class AircraftQueue:
   # Passenger Large = 3
 
   def systemBoot(self):
-    print("System Starting")
+    try:
+      print("System Starting")
+    except Exception as e:
+      print(e)
+      return
   
   def enqueue(self, AC):
-    return self.listOfAircraft.append(AC)
+    try:
+      return self.listOfAircraft.append(AC)
+    except Exception as e:
+      print(e)
+      return
     
   def dequeue(self):
-    myAircraft = self.listOfAircraft
-    if len(myAircraft) == 0:
-      print("Currently No Aircrafts")
-      return
+    try:
+      myAircraft = self.listOfAircraft
+      if len(myAircraft) == 0:
+        print("Currently No Aircrafts")
+        return
+        
+      removalIndex = 0
+      maxWeight = 0
+      for index, aircraft in enumerate(myAircraft):
+        currentType = aircraft.getType()
+        currentSize = aircraft.getSize()
+        currentWeight = self.typeWeights[currentType] + self.sizeWeights[currentSize]
+        if currentWeight > maxWeight:
+          maxWeight = currentWeight
+          removalIndex = index
       
-    removalIndex = 0
-    maxWeight = 0
-    for index, aircraft in enumerate(myAircraft):
-      currentType = aircraft.getType()
-      currentSize = aircraft.getSize()
-      currentWeight = self.typeWeights[currentType] + self.sizeWeights[currentSize]
-      if currentWeight > maxWeight:
-        maxWeight = currentWeight
-        removalIndex = index
-    
-    removed = myAircraft[removalIndex]
-    self.listOfAircraft.pop(removalIndex)
-    return removed
+      removed = myAircraft[removalIndex]
+      self.listOfAircraft.pop(removalIndex)
+      return removed
+    except Exception as e:
+      print(e)
+      return
 
   def getList(self):
     return self.listOfAircraft
@@ -50,13 +62,25 @@ class Aircraft:
     self.ACsize = ACsize
     
   def getName(self):
-    return self.ACname
+    try:
+      return self.ACname
+    except Exception as e:
+      print(e)
+      return
     
   def getType(self):
-    return self.ACtype
+    try:
+      return self.ACtype
+    except Exception as e:
+      print(e)
+      return
   
   def getSize(self):
-    return self.ACsize
+    try:
+      return self.ACsize
+    except Exception as e:
+      print(e)
+      return
 
 airport = AircraftQueue()
 airport.systemBoot()
@@ -104,4 +128,3 @@ print(removedAC9.getName())
 removedAC10 = airport.dequeue()
 print(removedAC10.getName())
 removedAC11 = airport.dequeue()
-print(removedAC11.getName())
